83df880a3b844c6037eee2d11c453f02e09b5514
altunoren <altunorenn@gmail.com>
2023-04-05 22:54:51 +0300

    Create sozcureklamkaldir.js

---
+++ sozcureklamkaldir.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Sözcü Reklam Kaldırma Sistemi
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Sözcü sitesindeki reklamları tek kod ile kaldırma
+// @author       İsmail Altunören
+// @match        https://www.sozcu.com.tr/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=sozcu.com.tr
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var intervalId = setInterval(function() {
+        // Get a list of all elements with class adInitRemove
+        var elementsToRemove = document.querySelectorAll('.ads, .stickyDesktopContainer');
+
+        // Loop through the list and remove each element
+        for (var i = 0; i < elementsToRemove.length; i++) {
+            elementsToRemove[i].remove();
+        }
+
+        // Stop checking if there are no more elements to remove
+        if (elementsToRemove.length === 0) {
+            clearInterval(intervalId);
+        }
+    }, 500);
+})();
